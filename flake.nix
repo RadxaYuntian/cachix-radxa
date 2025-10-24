a0b3813e4d1858ea45be31337ed6c78e3ee3601d
ZHANG Yuntian <yt@radxa.com>
2025-10-24 17:39:08 +0800

    feat: add minor packages
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- flake.nix
+++ flake.nix
@@ -56,10 +56,25 @@
       stable-linuxPackages_6_6_89_kernel = nixpkgs-unfree.linuxPackages_6_6_89.kernel;
       stable-linuxPackages_6_6_10_kernel_dev = nixpkgs-unfree.linuxPackages_6_6_10.kernel.dev;
       stable-linuxPackages_6_6_89_kernel_dev = nixpkgs-unfree.linuxPackages_6_6_89.kernel.dev;
+      stable-linuxPackages_6_6_89_kernel_r8125 = nixpkgs-unfree.linuxPackages_6_6_89.r8125;
+      stable-linuxPackages_6_6_89_kernel_cix_gpu_kernel = nixpkgs-unfree.linuxPackages_6_6_89.cix_gpu_kernel;
+      stable-linuxPackages_6_6_89_kernel_cix_isp_driver = nixpkgs-unfree.linuxPackages_6_6_89.cix_isp_driver;
+      stable-linuxPackages_6_6_89_kernel_cix_npu_driver = nixpkgs-unfree.linuxPackages_6_6_89.cix_npu_driver;
+      stable-linuxPackages_6_6_89_kernel_cix_vpu_driver = nixpkgs-unfree.linuxPackages_6_6_89.cix_vpu_driver;
+      stable-cix_gpu_firmware_2025_09 = nixpkgs-unfree.cix_gpu_firmware_2025_09;
+      stable-cix_vpu_firmware_2025_09 = nixpkgs-unfree.cix_vpu_firmware_2025_09;
+
       unstable-linuxPackages_6_6_10_kernel = nixpkgs-unstable-unfree.linuxPackages_6_6_10.kernel;
       unstable-linuxPackages_6_6_89_kernel = nixpkgs-unstable-unfree.linuxPackages_6_6_89.kernel;
       unstable-linuxPackages_6_6_10_kernel_dev = nixpkgs-unstable-unfree.linuxPackages_6_6_10.kernel.dev;
       unstable-linuxPackages_6_6_89_kernel_dev = nixpkgs-unstable-unfree.linuxPackages_6_6_89.kernel.dev;
+      unstable-linuxPackages_6_6_89_kernel_r8125 = nixpkgs-unstable-unfree.linuxPackages_6_6_89.r8125;
+      unstable-linuxPackages_6_6_89_kernel_cix_gpu_kernel = nixpkgs-unstable-unfree.linuxPackages_6_6_89.cix_gpu_kernel;
+      unstable-linuxPackages_6_6_89_kernel_cix_isp_driver = nixpkgs-unstable-unfree.linuxPackages_6_6_89.cix_isp_driver;
+      unstable-linuxPackages_6_6_89_kernel_cix_npu_driver = nixpkgs-unstable-unfree.linuxPackages_6_6_89.cix_npu_driver;
+      unstable-linuxPackages_6_6_89_kernel_cix_vpu_driver = nixpkgs-unstable-unfree.linuxPackages_6_6_89.cix_vpu_driver;
+      unstable-cix_gpu_firmware_2025_09 = nixpkgs-unstable-unfree.cix_gpu_firmware_2025_09;
+      unstable-cix_vpu_firmware_2025_09 = nixpkgs-unstable-unfree.cix_vpu_firmware_2025_09;
     } // mkPkgSet nixpkgs-unfree pkgList "stable"
     // mkPkgSet nixpkgs-unstable-unfree pkgListUnstable "unstable";
   };
