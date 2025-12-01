9b94fe8363147b6ee66745124c36e124db7e1387
ZHANG Yuntian <yt@radxa.com>
2025-12-01 10:41:20 +0800

    chore: tracking 25.11
    Signed-off-by: ZHANG Yuntian <yt@radxa.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:RadxaYuntian/nixos-hardware/sky1";
   };
